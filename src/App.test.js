9e379255a01dca9079a4ab88cbed29578d3e62b2
Jasmine <claridiva2000@gmail.com>
2020-08-08 23:10:25 -0500

    jest and enzyme added and working

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+//must be included in all test files or imported
+import { shallow, mount } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("Counter Testing", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App/>);
+  });
+
+  test("renders without crashing", () => {
+    expect(wrapper.find("body"));
+  });
+
+  // test("render a button w/ text of `increment` ", () => {
+  //   expect(wrapper.find("#increment-btn").text()).toBe("Increment");
+  // });
+
+  // test("render the initial value of state in a div", () => {
+  //   expect(wrapper.find("#counter-value").text()).toBe("0");
+  // });
+
+  // test("render the click even of increment button and increment couter value", () => {
+  //   wrapper.find("#increment-btn").simulate("click");
+  //   expect(wrapper.find("#counter-value").text()).toBe("1");
+  // });
+
+  // test("render the click even of decrement button and decrement couter value", () => {
+  //   wrapper.find("#increment-btn").simulate("click");
+  //   expect(wrapper.find("#counter-value").text()).toBe("1");
+  //   wrapper.find("#decrement-btn").simulate("click");
+  //   expect(wrapper.find("#counter-value").text()).toBe("0");
+  // });
+
+  // test("renders 0 if clicking decrement on 0 value", () => {
+  //   wrapper.find("#decrement-btn").simulate("click");
+  //   expect(wrapper.find("#counter-value").text()).toBe("0");
+  // });
+
+
 });
