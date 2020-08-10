11c6d7252c66a8bff3bebe4f0f8c8dd8a5738d77
Sam Kach <samwelmoyi@gmail.com>
2020-08-10 12:33:13 +0300

    added node-sass and enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('<App/>', () => {
+  test('render app', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('a').text()).toBe('Learn React');
+  });
 });
