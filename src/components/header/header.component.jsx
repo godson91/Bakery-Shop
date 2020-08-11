8a168ea219817155ee6c480c3a9ca280b8f95b66
Sam Kach <samwelmoyi@gmail.com>
2020-08-11 14:52:42 +0300

    top nav styling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as User } from '../../assets/user-alt-solid.svg';
 import { ReactComponent as Facebook } from '../../assets/facebook-square-brands.svg';
 import { ReactComponent as Instagram } from '../../assets/instagram-brands.svg';
+import { ReactComponent as ShoppingCart } from '../../assets/shopping-cart-solid.svg';
+
+import CustomButton from '../custom-button/custom-button.component';
 
 import './header.styles.scss';
 
@@ -18,6 +21,10 @@ const Header = () => {
           <Instagram />
         </div>
         <Link to="/">Logo</Link>
+        <div className="cart-order">
+          <CustomButton>Order Online</CustomButton>
+          <ShoppingCart />
+        </div>
       </div>
       <nav>
         <Link to="/">Home</Link>
