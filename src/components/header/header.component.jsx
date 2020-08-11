cef87d99b0a248b2d8a0756ae57dc1b39f93dee5
Sam Kach <samwelmoyi@gmail.com>
2020-08-11 16:56:48 +0300

    made navbar responsive in smaller devices

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import { ReactComponent as User } from '../../assets/user-alt-solid.svg';
 import { ReactComponent as Facebook } from '../../assets/facebook-square-brands.svg';
 import { ReactComponent as Instagram } from '../../assets/instagram-brands.svg';
 import { ReactComponent as ShoppingCart } from '../../assets/shopping-cart-solid.svg';
+import { ReactComponent as Bars } from '../../assets/bars-solid.svg';
 
 import CustomButton from '../custom-button/custom-button.component';
 
@@ -20,13 +21,19 @@ const Header = () => {
           <Facebook />
           <Instagram />
         </div>
-        <Link to="/">Logo</Link>
+        <Link to="/" className="logo">
+          Swayzer's Sweets
+        </Link>
         <div className="cart-order">
           <CustomButton>Order Online</CustomButton>
           <ShoppingCart />
         </div>
       </div>
       <nav>
+        <div>
+          <span>Swayzer's Sweets</span>
+          <Bars />
+        </div>
         <Link to="/">Home</Link>
         <Link to="/about">About Us</Link>
         <Link to="/shop">Shop</Link>
