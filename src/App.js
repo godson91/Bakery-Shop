c1bf1e4205bf901d9f00b5c72cf257cc145795bf
Sam Kach <samwelmoyi@gmail.com>
2020-08-11 15:24:31 +0300

    nav-bar/desktop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
+// Components
 import Header from './components/header/header.component';
 
+//Pages
+import Homepage from './pages/homepage/homepage.component';
+
 class App extends Component {
-  state = {};
   render() {
     return (
       <div className="App">
         <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+        </Switch>
       </div>
     );
   }
