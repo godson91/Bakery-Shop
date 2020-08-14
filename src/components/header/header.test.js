80df283c55b7cac8ecb92ff70b9f45dfb8c63d3b
Sam Kach <samwelmoyi@gmail.com>
2020-08-14 18:30:26 +0300

    nav dropdown on mobile

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 
