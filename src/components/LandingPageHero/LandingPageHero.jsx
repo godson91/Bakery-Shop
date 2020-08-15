e5f6978ca3c59336558443e5d052203967894635
Jasmine <claridiva2000@gmail.com>
2020-08-15 16:09:37 -0500

    added netlify redirects

---
+++ src/components/LandingPageHero/LandingPageHero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './landingpagehero.styles.scss'
+
+function LandingPageHero() {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+export default LandingPageHero
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Hero from './../../components/LandingPageHero/LandingPageHero'
 import './homepage.styles.scss';
 
 const HomePage = () => {
-  return <div className="homepage"></div>;
+  return <div className="homepage">
+<Hero/>
+  </div>;
 };
 
 export default HomePage;
