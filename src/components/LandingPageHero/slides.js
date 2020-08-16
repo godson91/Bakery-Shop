fc9d538aa17774d644340d3ef07177ec2bdfd625
Jasmine <claridiva2000@gmail.com>
2020-08-16 00:24:12 -0500

    hero element complete

---
+++ src/components/LandingPageHero/slides.js
@@ -0,0 +1,56 @@
+export default [
+	{
+		title: 'Vulputate Mollis Ultricies',
+		description:
+			'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+		button: 'Read More',
+		image: 'https://firebasestorage.googleapis.com/v0/b/bakery-dev-server.appspot.com/o/images%2Fbanana%20pudding.png?alt=media&token=319d993f-6ebf-435d-bbd0-ca2c5e96d130',
+		user: 'Daniel',
+		userProfile: 'https://s7.postimg.cc/abavelo3v/1_3x.png',
+	},
+	{
+		title: 'Tortor Dapibus',
+		description:
+			'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.',
+		button: 'Discover',
+		image: 'https://firebasestorage.googleapis.com/v0/b/bakery-dev-server.appspot.com/o/images%2Fmango_cheesecake.jpg?alt=media&token=b2ca7c3f-0197-435b-b17b-4687df8a8fbd',
+		user: 'Samantha',
+		userProfile: 'https://s7.postimg.cc/ujy8zz7vv/5_3x.png',
+	},
+	{
+		title: 'Phasellus volutpat metus',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.',
+		button: 'Buy now',
+		image: 'https://firebasestorage.googleapis.com/v0/b/bakery-dev-server.appspot.com/o/images%2Fbrownie_cake.jpg?alt=media&token=68896693-8bf8-4655-a4f3-88992ea32b15',
+		user: 'Michael',
+		userProfile: 'https://s7.postimg.cc/6exjimijv/3_3x.png',
+	},
+	{
+		title: 'Ultricies Vulputate Mollis',
+		description:
+			'Aenean eu leo quam. Pellentesque ornare sem lacinia  nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
+		button: 'Read More',
+		image: 'https://firebasestorage.googleapis.com/v0/b/bakery-dev-server.appspot.com/o/images%2Ftart.jpg?alt=media&token=a0943076-4c69-4285-b432-38f6161d65b6',
+		user: 'Jessica',
+		userProfile: 'https://s7.postimg.cc/7ihnu80ij/4_3x.png',
+	},
+	{
+		title: 'Odo Aenean Quam Tortor',
+		description:
+			'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		button: 'Discover',
+		image: 'https://firebasestorage.googleapis.com/v0/b/bakery-dev-server.appspot.com/o/images%2Fbrownie.jpg?alt=media&token=6bd8070f-69f9-48fb-a3e8-008f78acbc7a',
+		user: 'William',
+		userProfile: 'https://s7.postimg.cc/f9ydt4zmj/2_3x.png',
+	},
+	{
+		title: 'Volutpat Aenean metus',
+		description:
+			'quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentumconsectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis.',
+		button: 'Buy now',
+		image: 'https://firebasestorage.googleapis.com/v0/b/bakery-dev-server.appspot.com/o/images%2Fcookieplate.jpg?alt=media&token=87eda7be-c9d5-4b0a-a773-469961b6da12',
+		user: 'Katerina',
+		userProfile: 'https://s7.postimg.cc/hsk2th5tn/6_3x.png',
+	},
+];
