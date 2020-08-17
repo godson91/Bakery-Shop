2e3862629c8f736202fa9764558de8408877c7e5
Sam Kach <samwelmoyi@gmail.com>
2020-08-17 11:44:23 +0300

    fixed nav dropdown

--- src/components/LandingPageHero/LandingPageHero.jsx
+++ src/components/LandingPageHero/LandingPageHero.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
+import React from 'react';
 import Slider from 'react-animated-slider';
-import content from './slides'
-import customBtn from '../custom-button/custom-button.component'
-import './landingpagehero.styles.scss'
+import content from './slides';
+import CustomButton from '../custom-button/custom-button.component';
+import './landingpagehero.styles.scss';
 
 function LandingPageHero() {
-    return (
-        <div>
-           <Slider>
-{content.map((item, index) => (
-	<div
-        key={index}
-        className="slideBG"
-        style={{ background: `url('${item.image}') no-repeat center center`, 
-        backgroundSize: `cover`,
-        outline: '200px solid rgba(0, 0, 0, 0.4)',
-        outlineOffset: '-200px',
-        overflow: 'hidden'
-     }}
-	>
-		<div className="center">
-			<h1>{item.title}</h1>
-			<p>{item.description}</p>
-			<button className="sliderbtn">Order Now</button>
-		</div>
-	</div>
-))}
-</Slider>
-      </div>
-    )
+  return (
+    <div>
+      <Slider>
+        {content.map((item, index) => (
+          <div
+            key={index}
+            className="slideBG"
+            style={{
+              background: `url('${item.image}') no-repeat center center`,
+              backgroundSize: `cover`,
+              outline: '200px solid rgba(0, 0, 0, 0.4)',
+              outlineOffset: '-200px',
+              overflow: 'hidden',
+            }}
+          >
+            <div className="center">
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+              <CustomButton>Order Now</CustomButton>
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
 
-
-
-export default LandingPageHero
+export default LandingPageHero;
