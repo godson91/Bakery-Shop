e2f4551d3f18f15888ad1f340137bc315812b3ce
Sam Kach <samwelmoyi@gmail.com>
2020-08-17 16:26:48 +0300

    code organisation

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Hero from './../../components/LandingPageHero/LandingPageHero'
+import Hero from './../../components/LandingPageHero/LandingPageHero';
 import './homepage.styles.scss';
 
 const HomePage = () => {
-  return <div className="homepage">
-<Hero/>
-  </div>;
+  return (
+    <div className="homepage">
+      <Hero />
+    </div>
+  );
 };
 
 export default HomePage;
